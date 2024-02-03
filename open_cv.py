2ff97b5186d299389fb7201debe5a93dcdf51fef
JusticeJJackson <JusticeJackson@vt.edu>
2024-02-03 15:00:32 -0500

    added tracking to openCV

--- open_cv.py
+++ open_cv.py
@@ -3,7 +3,7 @@ from cvzone.PoseModule import PoseDetector
 import time
 
 # TO BE SET DURING DEVELOPMENT
-debugging = True
+debugging = False
 
 last_printed_time = time.time()
 
@@ -33,61 +33,146 @@ def detect_bounding_box_for_faces(vid, show_bbox=False):
 
 # while loop to continuously read frames from the webcam
 while True:
+    current_time = time.time()
 
     result, video_frame = webcam.read()  # read frames from the video
 
     if result is False:
+        print("There was an error reading the frame")
         break  # terminate the loop if the frame is not read successfully
 
-    # img = detector.findPose(video_frame)
+    detector.findPose(video_frame, draw=False)
 
+    lmList, bboxInfo = detector.findPosition(
+        video_frame, draw=False, bboxWithHands=False
+    )
 
-    img, bboxInfo = detector.findPosition(video_frame)
-    
-    if bboxInfo is not None:
-        
-        keypoints = bboxInfo["keypoints"]
-        
-        left_shoulder = keypoints[11]
-        right_shoulder = keypoints[12]
-        left_hip = keypoints[23]
-        right_hip = keypoints[24]
+    # if current_time - last_printed_time >= 5:
+    #     print(lmList)
+    #     print("---------------------------------------")
+    #     last_printed_time = current_time
+
+    if lmList:
+
+        left_shoulder = lmList[11]
+        right_shoulder = lmList[12]
+
+        left_elbow = lmList[13]
+        right_elbow = lmList[14]
+
+        left_hip = lmList[23]
+        right_hip = lmList[24]
+
+        left_ankle = lmList[27]
+        right_ankle = lmList[28]
 
         # Unpacking coordinates for each keypoint
-        x_left_shoulder, y_left_shoulder = left_shoulder
-        x_right_shoulder, y_right_shoulder = right_shoulder
-        x_left_hip, y_left_hip = left_hip
-        x_right_hip, y_right_hip = right_hip
+        x_left_shoulder, y_left_shoulder = left_shoulder[0], left_shoulder[1]
+        x_right_shoulder, y_right_shoulder = right_shoulder[0], right_shoulder[1]
+
+        x_left_elbow, y_left_elbow = left_elbow[0], left_elbow[1]
+        x_right_elbow, y_right_elbow = right_elbow[0], right_elbow[1]
+
+        x_left_hip, y_left_hip = left_hip[0], left_hip[1]
+        x_right_hip, y_right_hip = right_hip[0], right_hip[1]
+
+        x_left_ankle, y_left_ankle = left_ankle[0], left_ankle[1]
+        x_right_ankle, y_right_ankle = right_ankle[0], right_ankle[1]
 
         # Printing the coordinates of the keypoints
-        print("Left Shoulder:", left_shoulder)
-        print("Right Shoulder:", right_shoulder)
-        print("Left Hip:", left_hip)
-        print("Right Hip:", right_hip)
-        
-            # Drawing lines between keypoints for demonstration
-        cv2.line(video_frame, left_shoulder, left_hip, (0, 255, 0), 2)
-        cv2.line(video_frame, right_shoulder, right_hip, (0, 255, 0), 2)
+
+        # print("RELEVANT JOINT INFO")
+        # print(" ")
+        # print("Left Shoulder:", left_shoulder)
+        # print("Right Shoulder:", right_shoulder)
+        # print("Left Hip:", left_hip)
+        # print("Right Hip:", right_hip)
+        # print("Left Ankle:", left_ankle)
+        # print("Right Ankle:", right_ankle)
+
+        # Drawing lines between keypoints for demonstration
+        cv2.line(
+            video_frame,
+            (x_left_shoulder, y_left_shoulder),
+            (x_left_hip, y_left_hip),
+            (0, 255, 0),
+            2,
+        )
+        cv2.line(
+            video_frame,
+            (x_right_shoulder, y_right_shoulder),
+            (x_right_hip, y_right_hip),
+            (0, 255, 0),
+            2,
+        )
+        cv2.line(
+            video_frame,
+            (x_left_hip, y_left_hip),
+            (x_left_ankle, y_left_ankle),
+            (255, 255, 0),
+            5,
+        )
+        cv2.line(
+            video_frame,
+            (x_right_hip, y_right_hip),
+            (x_right_ankle, y_right_ankle),
+            (255, 255, 0),
+            5,
+        )
+        cv2.line(
+            video_frame,
+            (x_left_shoulder, y_left_shoulder),
+            (x_left_elbow, y_left_elbow),
+            (255, 0, 255),
+            10,
+        )
+        cv2.line(
+            video_frame,
+            (x_right_shoulder, y_right_shoulder),
+            (x_right_elbow, y_right_elbow),
+            (255, 0, 255),
+            10,
+        )
 
         # Drawing circles at keypoints for visualization
-        cv2.circle(video_frame, left_shoulder, 5, (0, 0, 255), cv2.FILLED)
-        cv2.circle(video_frame, right_shoulder, 5, (0, 0, 255), cv2.FILLED)
-        cv2.circle(video_frame, left_hip, 5, (0, 0, 255), cv2.FILLED)
-        cv2.circle(video_frame, right_hip, 5, (0, 0, 255), cv2.FILLED)
+        cv2.circle(
+            video_frame, (x_left_shoulder, y_left_shoulder), 50, (0, 0, 255), cv2.FILLED
+        )
+        cv2.circle(
+            video_frame,
+            (x_right_shoulder, y_right_shoulder),
+            50,
+            (0, 0, 255),
+            cv2.FILLED,
+        )
+        cv2.circle(video_frame, (x_left_hip, y_left_hip), 50, (0, 0, 255), cv2.FILLED)
+        cv2.circle(video_frame, (x_right_hip, y_right_hip), 50, (0, 0, 255), cv2.FILLED)
+        cv2.circle(
+            video_frame, (x_left_ankle, y_left_ankle), 20, (255, 255, 255), cv2.FILLED
+        )
+        cv2.circle(
+            video_frame, (x_right_ankle, y_right_ankle), 20, (255, 255, 255), cv2.FILLED
+        )
+        cv2.circle(
+            video_frame, (x_left_elbow, y_left_elbow), 20, (255, 0, 255), cv2.FILLED
+        )
+        cv2.circle(
+            video_frame, (x_right_elbow, y_right_elbow), 20, (255, 0, 255), cv2.FILLED
+        )
 
-        
     # TRUE TO SHOW BOX FOR DEBUGGING
     faces = detect_bounding_box_for_faces(
-        video_frame, True
+        video_frame, False
     )  # detect the bounding box around the face
 
     cv2.imshow("Video Frame", video_frame)  # display the frame
+
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
     if debugging:
         # print every 3 second
-        current_time = time.time()
+
         if current_time - last_printed_time >= 3:
             print(
                 f"Face x: {face_x_coord}, Face y: {face_y_coord}, Face width: {face_width}, Face height: {face_height}"
