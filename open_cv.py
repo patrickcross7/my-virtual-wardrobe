8864750f051922665870dc8a5018e7041c5b02ec
JusticeJJackson <JusticeJackson@vt.edu>
2024-02-03 11:51:04 -0500

    adding body point detection to image

--- open_cv.py
+++ open_cv.py
@@ -1,12 +1,17 @@
 import cv2
 
+from cvzone.PoseModule import PoseDetector
+
+
 face_classifier = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 )
 
 webcam = cv2.VideoCapture(0)
+detector = PoseDetector()
 
 
+# TO BE USED FOR ADDING A BLACK BOX AROUND THE FACE
 def detect_bounding_box(vid):
     gray_image = cv2.cvtColor(vid, cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray_image, 1.1, 5, minSize=(40, 40))
@@ -23,14 +28,11 @@ while True:
     if result is False:
         break  # terminate the loop if the frame is not read successfully
 
-    faces = detect_bounding_box(
-        video_frame
-    )  # apply the function we created to the video frame
+    img = detector.findPose(video_frame)
 
-    cv2.imshow(
-        "My Face Detection Project", video_frame
-    )  # display the processed frame in a window named "My Face Detection Project"
+    lmList, bboxInfo = detector.findPosition(img)
 
+    cv2.imshow("Video Frame", img)  # display the frame
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
